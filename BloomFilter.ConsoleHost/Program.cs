c84b8462fcf091122e4099ab6d1ea70fe994d024
Valeriy Kovetskiy <kovetskiy.valeriy@gmail.com>
2021-09-22 12:09:06 +0300

    Init

---
+++ BloomFilter.ConsoleHost/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using RedisBloom.Extensions;
+using StackExchange.Redis;
+
+namespace BloomFilter.ConsoleHost
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var conn = await ConnectionMultiplexer.ConnectAsync("127.0.0.1");
+            var db = conn.GetDatabase(0);
+
+
+            //await CheckErrorRate(db);
+            await GenerateGuids("guids.txt", 1000000);
+            await WriteGuids(db, "guids.txt");
+
+            // Console.WriteLine(await db.ExistsAsync("1"));
+        }
+
+        static async Task GenerateGuids(string filename, int size)
+        {
+            var guids = new List<string>();
+
+            for (int i = 0; i < size; i++)
+            {
+                guids.Add(Guid.NewGuid().ToString());
+            }
+            
+            await File.WriteAllLinesAsync(filename, guids);
+        }
+
+        static async Task WriteGuids(IDatabaseAsync db, string filename)
+        {
+            var guids = await File.ReadAllLinesAsync(filename);
+            
+            foreach (var guid in guids)
+            {
+                await db.AddAsync(guid);
+            }
+        }
+
+        static async Task CheckErrorRate(IDatabaseAsync db)
+        {
+            var errors = 0;
+            var size = 10000;
+            
+            await GenerateGuids("fake-guids.txt", size);
+            var guids = await File.ReadAllLinesAsync("fake-guids.txt");
+
+            var timer = new Stopwatch();
+            timer.Start();
+            foreach (var guid in guids)
+            {
+                var isExist = await db.ExistsAsync(guid);
+                if (isExist)
+                {
+                    //Console.WriteLine($"This GUID already exists - {fakeGuid}");
+                    errors++;
+                }
+            }
+            timer.Stop();
+
+            Console.WriteLine($"Elapsed ms - {timer.ElapsedMilliseconds}ms");
+            Console.WriteLine($"Errors found - {errors}");
+            Console.WriteLine($"Error rate - {(double)size / (size - errors)}");
+        }
+    }
+}
---
+++ RedisBloom.Extensions/BloomFilter.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace RedisBloom.Extensions
+{
+    public static class BloomFilter
+    {
+        public static async Task AddAsync(this IDatabaseAsync db, string data)
+        {
+            await db.ExecuteAsync("BF.ADD", "test", data);
+        }
+
+        public static async Task<bool> ExistsAsync(this IDatabaseAsync db,string data)
+        {
+            return (bool)await db.ExecuteAsync("BF.EXISTS", "test", data);
+        }
+    }
+}
